7ebc74a2ecf2ba532deb7a4c662fb95ef77f6e31
brendon <brendonzimmerbusiness@gmail.com>
2021-06-25 07:43:44 -0700

    Update package.json and add definition to 'trunacte'

--- src/helpers/truncate.ts
+++ src/helpers/truncate.ts
@@ -1,3 +1,7 @@
+/**
+ * truncates number at the decimal point (like an int).
+ * @internal
+ */
 export function truncate(num: number) {
   const str = num.toString();
   return +str.slice(0, str.indexOf("."));
